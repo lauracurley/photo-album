4aa11d7b7382f99165f5b889fda546c3932cf32c
Laura Curley <info@lauracurley.com>
2016-03-29 07:28:17 -0700

    Implement input view and rating dropdown in feature view

---
+++ js/models/AppModel.js
@@ -0,0 +1,38 @@
+var PhotoEntry = Backbone.Model.extend({
+
+  defaults: {
+    title: 'Success!',
+    url: '../img/success.jpeg',
+    rating: 0
+  },
+
+  initialize: function() {},
+
+  toggleFeature: function() {
+    // when a photo entry view is clicked, set the feature photo
+    this.trigger('toggleFeature', this);
+  },
+
+  changeRating: function(self, rating) {
+    console.log(rating);
+    this.model.set('rating', rating);
+    this.trigger('changeRating', this);
+  }
+
+});
+    // var isImperial = this.get('unit') === '°F';
+
+    // if (isImperial) {
+    //   var celsius = (this.get('weather') - 32) * (5 / 9);
+    //   this.set({
+    //     'unit': '°C',
+    //     'weather': celsius.toFixed(2)
+    //   });
+    // }
+    // else {
+    //   var fahrenheit = (this.get('weather') / (5 / 9)) + 32;
+    //   this.set({
+    //     'unit': '°F',
+    //     'weather': fahrenheit.toFixed(2)
+    //   });
+    // }
